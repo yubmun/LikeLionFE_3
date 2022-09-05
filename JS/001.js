907a9b721e66c0e56a6bc659aefb7e0b28d89804
junyub.lee <dlwnsduq23@gmail.com>
2022-09-05 23:26:21 +0900

    라이언 클론코딩

--- JS/001.js
+++ JS/001.js
@@ -7,3 +7,15 @@ for (let i = 0; i < word.length; i++) {
   count++;
 }
 console.log(count);
+
+let x = 10;
+
+x = 10 + '';
+
+console.log(typeof x, x);
+
+let y = typeof x;
+console.log(y);
+if(y === 'string'){
+  console.log('string이 맞네');
+}
