9a5c5c6fa1fdcd45a65df26e33e7c7168597f388
junyub.lee <dlwnsduq23@gmail.com>
2022-10-07 00:14:26 +0900

    reduce에 대해서

--- JS강의/b.js
+++ JS강의/b.js
@@ -3,3 +3,34 @@ var 변수 = 50;
 function 함수(a,b) {
   return a*b;
 }
+
+let arr = [1,2,3,4,5];
+let result = arr.reduce((a,b)=>a+b);
+console.log(result);
+
+let result2 = arr.reduce(((a,b)=>a+b), 10);
+console.log(result2);
+
+let names = ['Alice','Bob','Tiff','Bruce','Alice'];
+let result3 = names.reduce(function(allNames, name){
+  if(name in allNames){
+    allNames[name]++;
+  } else {
+    allNames[name] = 1;
+  }
+  return allNames;
+}, {});
+console.log(result3);
+
+
+const numbers = [2, -5, -123, 59, -5480, 24, 0, -69, 349, 3];
+const result4 = numbers.reduce((a,b)=>{
+  if(b<0){
+    a[0]++;
+  } else if(b >0){
+    a[1]++;
+  }
+  return a;
+}, [0,0]);
+console.log(result4);
+
---
+++ JS강의/test.js
@@ -0,0 +1,30 @@
+var people = [
+  { name: 'Alice', age: 21 },
+  { name: 'Max', age: 20 },
+  { name: 'Jane', age: 20 }
+];
+
+function groupBy(objectArray, property) {
+  return objectArray.reduce(function (acc, obj) {
+    var key = obj[property];
+    
+    if (!acc[key]) {
+      acc[key] = [];
+    }
+    acc[key].push(obj);
+    console.log(acc , '두번째');
+    console.log(acc[key], '첫번째');
+    
+    return acc;
+  }, {});
+}
+
+var groupedPeople = groupBy(people, 'age');
+// groupedPeople is:
+// {
+//   20: [
+//     { name: 'Max', age: 20 },
+//     { name: 'Jane', age: 20 }
+//   ],
+//   21: [{ name: 'Alice', age: 21 }]
+// }
