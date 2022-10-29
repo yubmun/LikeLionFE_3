c1665fc182ac6710b41c9008550391a498433d61
junyub.lee <dlwnsduq23@gmail.com>
2022-10-29 23:25:26 +0900

    모스부호

---
+++ algorithm/모스부호.js
@@ -0,0 +1,22 @@
+morse = { 
+  '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+  '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+  '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+  '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+  '-.--':'y','--..':'z'
+}
+
+function solution(letter) {
+  var answer = '';
+  let arr = letter.split(' ');
+  for(let i=0; i<arr.length; i++){
+      for(const sign in morse){
+          if(arr[i] === sign){
+              answer += morse[sign];
+          }
+      }
+  }
+  return answer;
+}
+
+// for in이 for문 안으로 들어와야 정답처리가 되는 이유에 대해 잘 모르겠음.
