bd4f5616d5782d5ad1651185a29391ca48cb6dd9
junyub.lee <dlwnsduq23@gmail.com>
2022-11-22 15:59:17 +0900

    1122

---
+++ redux/004/my-app/src/App.js
@@ -0,0 +1,11 @@
+import GoodsCounter from "./components/GoodsCounter";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+      <GoodsCounter/>
+    </div>
+  );
+}
+export default App;
